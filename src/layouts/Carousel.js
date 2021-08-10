46084f79a0d0ccab4ab7d65014ca9a4d881bc9e2
Jihye Son <jiiiheeey@gmail.com>
2021-08-10 23:51:28 +0900

    21-08-10 carousel 수정

--- src/layouts/Carousel.js
+++ src/layouts/Carousel.js
@@ -1,44 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import interestList from "../assets/data/interestList.json";
+import slide from "../assets/img/slide.jpg";
 import "./Carousel.css";
 function Carousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideRef = useRef(null);
+  const TOTAL_SLIDES = 2; //3장
+  const NextSlide = () => {
+    if (currentSlide >= TOTAL_SLIDES) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const PrevSlide = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(TOTAL_SLIDES);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+  useEffect(() => {
+    slideRef.current.style.transition = "all 0.5s ease-in-out";
+    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 에니메이션
+  }, [currentSlide]);
   return (
     <>
-      <div className="carousel">
-        <div className="carousel-container">
-          <input name="carousel" type="radio" id="carousel1" className="carousel-items" checked />
-          <input name="carousel" type="radio" id="carousel2" className="carousel-items" />
-          <input name="carousel" type="radio" id="carousel3" className="carousel-items" />
-
-          <div className="slidewrap">
-            <ul className="slidelist">
-              <li>
-                <a>
-                  <label for="carousel3" className="left"></label>
-                  <div>11111</div>
-                  <label for="carousel2" className="right"></label>
-                </a>
-              </li>
-              <li>
-                <a>
-                  <label for="carousel2" className="left"></label>
-                  <div>222</div>
-                  <label for="carousel1" className="right"></label>
-                </a>
-              </li>
-              <li>
-                <a>
-                  <label for="carousel1" className="left"></label>
-                  <div>333</div>
-                  <label for="carousel3" className="right"></label>
-                </a>
-              </li>
-            </ul>
+      <div className="container">
+        <div className="text">
+          <h1>Carousel</h1>
+          <p>
+            {currentSlide + 1} / {TOTAL_SLIDES + 1}
+          </p>
+        </div>
+        <div className="sildercontainer" ref={slideRef}>
+          <div className="slide">
+            <img src={slide} width="500px" alt="" />
+            <h3>안녕안녕</h3>
+          </div>
+          <div className="slide">
+            <img src={slide} width="500px" alt="" />
+            <h3>내이름은</h3>
+          </div>
+          <div className="slide">
+            <img src={slide} width="500px" alt="" />
+            <h3>지혜야</h3>
           </div>
         </div>
-        <div className="carousel-button">버튼1</div>
-        <div className="carousel-button">버튼2</div>
-        <div className="carousel-button">버튼3</div>.
+        <div className="center">
+          <div className="button" onClick={PrevSlide}>
+            Prev
+          </div>
+          <div className="button" onClick={NextSlide}>
+            Next
+          </div>
+        </div>
       </div>
     </>
   );
