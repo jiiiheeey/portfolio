5ef755c2d22af21a5e6e647505164ba9339039f3
Jihye Son <jiiiheeey@gmail.com>
2021-08-17 23:50:01 +0900

    2021-08-17 scroll 로직 수정

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -5,17 +5,21 @@ import Myphoto from "../assets/img/profile_jihye.jpg";
 import Profile from "../assets/data/profile.json";
 import Carousel from "../layouts/Carousel";
 function Home() {
+  const headLineText = "Welcome";
   const boxText = "개발자 손지혜의 홈페이지";
-  const [text, setText] = useState("");
+  const [typingText, setTypingText] = useState("");
   const [count, setCount] = useState(0);
   const [goBtn, setGoBtn] = useState(false);
-  const [position, setPosition] = useState({ currentY: 0, headerBarY: 0, headerBarHeight: 0 });
+  // const [position, setPosition] = useState({ currentY: 0, headerBarY: 0 });
   const [topBar, setTopBar] = useState(false);
+  // const [positionBar, setPositionBar] = useState(300);
+  const [scrollUp, setScrollUp] = useState(false);
+  const [prevScroll, setPrevScroll] = useState(0);
 
   useEffect(() => {
     //   typing 효과
     const interval = setInterval(() => {
-      setText(text + boxText[count]);
+      setTypingText(typingText + boxText[count]);
       setCount(count + 1);
     }, 120);
     if (count === boxText.length) {
@@ -29,34 +33,41 @@ function Home() {
     return () => {
       window.removeEventListener("scroll", onScroll);
     };
-  }, []);
+  }, [prevScroll]);
 
   const onScroll = () => {
-    setPosition(window.scrollY);
-    let headerBarHeight = document.getElementById("header-bar").getBoundingClientRect().height;
-    let headerBarYPoint = headerBarHeight + document.getElementById("header-bar").getBoundingClientRect().top;
+    prevScroll > window.scrollY ? setScrollUp(true) : setScrollUp(false);
 
-    window.scrollY > headerBarYPoint ? setTopBar(true) : setTopBar(false);
+    // let headerBarYPoint = headerBarHeight + document.getElementById("header-bar").getBoundingClientRect().top;
+
+    // window.scrollY > headerBarYPoint ? setTopBar(true) : setTopBar(false);
+    // window.screenY > headerBarYPoint ? setPositionBar(window.scrollY) : setPosition(300);
+    // setPosition({ currentY: window.scrollY, headerBarY: headerBarHeight + document.getElementById("header-bar").getBoundingClientRect().top });
+    // position.currentY > position.headerBarY ? setTopBar(true) : setTopBar(false);
+    setPrevScroll(window.scrollY);
   };
 
   const onClickGoButton = () => {
-    let headerBarHeight = document.getElementById("header-bar").getBoundingClientRect().height;
-    let bodyYPoint = -headerBarHeight + document.getElementById("body").getBoundingClientRect().top;
+    // let headerBarHeight = document.getElementById("header-bar").getBoundingClientRect().height;
+
+    let bodyYPoint = document.getElementById("body").getBoundingClientRect().top;
     window.scrollTo({ top: bodyYPoint, left: 0, behavior: "smooth" });
   };
-  let positionBar;
-  position > "50vh" ? (positionBar = position) : (positionBar = 300);
+
   return (
     <>
       <div className="home">
-        <div className={topBar ? "home-header-bar home-header-bar-top" : "home-header-bar"} id="header-bar" style={{ marginTop: positionBar }}>
-          <div className="header-bar-text">{text}</div>
+        {scrollUp && <div className="home-header-bar-top">{headLineText}</div>}
+        <div className="home-header-bar" id="header-bar">
+          <div className="header-bar-text">{typingText}</div>
 
           {!topBar && (
             <div className="header-bar-go-button">
-              <div className="icon" onClick={onClickGoButton}>
-                <i class="fas fa-angle-double-down"></i>
-              </div>
+              {goBtn && (
+                <div className="click-icon" onClick={onClickGoButton}>
+                  CLICK <i class="fas fa-angle-double-down"></i>
+                </div>
+              )}
             </div>
           )}
         </div>
